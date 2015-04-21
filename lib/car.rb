3fde4d75622133fc92669ae769d406d3e34efed0
Annie Pennell <annie@anniepennell.com>
2015-04-20 17:33:47 -0700

    completed exercise

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,16 @@
 # Add Car class here
+# car class that can accelerate to a certain speed
+
+class Car
+  attr_reader :speed
+
+  # set initial speed to 0 when instantiating a car
+  def initialize()
+    @speed = 0
+  end
+
+  # set new speed to speed called when accelerate method is called
+  def accelerate(new_speed)
+    @speed = new_speed
+  end
+end
--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,31 @@
 require_relative 'car'
 
 # Add Race class here
+class Race
+  attr_reader :car1, :car2
+
+  def initialize()
+    @cars = []
+    ready_go
+  end
+
+  def random_speed
+    (rand * 125).round
+  end
+
+  def ready_go
+    @cars << Car.new
+    @cars << Car.new
+
+    @cars.first.accelerate(random_speed)
+    @cars.last.accelerate(random_speed)
+  end
+
+  def winner
+    @cars.sort_by(&:speed).last
+  end
+
+  def loser
+    @cars.sort_by(&:speed).first
+  end
+end
